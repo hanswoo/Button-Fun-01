6b8a0fb5df2ebfabc83805d37546f4f0c22c0f27
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-14 12:41:51 +0900

    add button change

--- Button Fun 01/ViewController.swift
+++ Button Fun 01/ViewController.swift
@@ -6,21 +6,33 @@
 //  Copyright © 2018년 ksh. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    var flag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.yellow
-        
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.cyan
     }
-
+    
+    
+    @IBAction func buttonpressed(_ sender: Any) {
+    if flag == 0 {
+            view.backgroundColor = UIColor.yellow
+            flag = 1
+        }else if flag == 1{
+            view.backgroundColor = UIColor.black
+            flag = 0
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
